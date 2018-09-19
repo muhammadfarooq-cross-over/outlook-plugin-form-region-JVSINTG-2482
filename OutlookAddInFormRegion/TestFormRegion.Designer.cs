c41c3841624452222515228ba43176aee5499359
muhammadfarooq-cross-over <muhammad.farooq@aurea.com>
2018-09-19 19:00:10 +0500

    initial

---
+++ OutlookAddInFormRegion/TestFormRegion.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace OutlookAddInFormRegion
+{
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class TestFormRegion : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public TestFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Test Text";
+            // 
+            // TestFormRegion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "TestFormRegion";
+            this.FormRegionShowing += new System.EventHandler(this.TestFormRegion_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.TestFormRegion_FormRegionClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        #region Form Region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "TestFormRegion";
+            manifest.FormRegionType = Microsoft.Office.Tools.Outlook.FormRegionType.Adjoining;
+            manifest.ShowReadingPane = false;
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+
+        public partial class TestFormRegionFactory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public TestFormRegionFactory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                TestFormRegion.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.TestFormRegionFactory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                TestFormRegion form = new TestFormRegion(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+    partial class WindowFormRegionCollection
+    {
+        internal TestFormRegion TestFormRegion
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(TestFormRegion))
+                        return (TestFormRegion)item;
+                }
+                return null;
+            }
+        }
+    }
+}
